a5ce6ac5c31b5e7b33eee711c0d3fc1243fb3cdd
nayoung2da <162244089+nayoung2da@users.noreply.github.com>
2024-03-06 06:32:46 +0000

    ppp20240306

---
+++ lec20240306/lec01.py
@@ -0,0 +1,5 @@
+print("Hello World")
+print("안녕 세상")
+print("...^^")
+print("故事成語")
+print('Hello world')
---
+++ lec20240306/lec02.py
@@ -0,0 +1,6 @@
+name = "우나영"
+department="스마트팜학과"
+university="전북대학교"
+
+print("{} {} ({})님 안녕하세요!".format(university, department, name))
+print("{}님 좋은 하루되세요~".format(name))
