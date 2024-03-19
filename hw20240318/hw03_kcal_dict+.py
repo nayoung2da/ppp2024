7a54d74a417865488ecc4ee88c454b7845d9a536
nayoung2da <162244089+nayoung2da@users.noreply.github.com>
2024-03-19 04:25:38 +0000

    hw20240318

---
+++ hw20240318/hw03_kcal_dict+.py
@@ -0,0 +1,9 @@
+calories = {"한라봉":0.5, "딸기":0.34, "바나나":0.77, "감":0.51, "망고":0.61, "리치":0.61, "복숭아":0.49, "블루베리":0.48, "사과":0.58}
+
+fruits = input("먹은 과일의 이름을 입력하세요. : ")
+g = int(input("{} 섭취량(g기준)을 입력하세요. : ".format(fruits)))
+
+if fruits in calories:
+    total_calories = calories[fruits] * g
+    print("=" * 12, "결과", "=" * 12)
+    print("{} {}g의 칼로리는 {}kcal입니다.".format(fruits, g, total_calories))
