28808becc2d9a9fd4474ad5764af2439a9b0d089
nayoung2da <162244089+nayoung2da@users.noreply.github.com>
2024-04-08 06:33:57 +0000

    lec20240408

---
+++ lec20240408/lec02_weather_file.py
@@ -0,0 +1,28 @@
+def read_tmax(filename):
+    results = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            tokens = line.split(",")
+            tmax = float(tokens[3])
+            results.append(tmax)
+    return results
+
+def read_tmin(filename):
+    results = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            tokens = line.split(",")
+            tmin = float(tokens[5])
+            results.append(tmin)
+    return results
+
+def main():
+    tmax = read_tmax("lec20240408/weather(146)_2022-2022.csv")
+    tmin = read_tmin("lec20240408/weather(146)_2022-2022.csv")
+    print(f"연 최고 온도(max(tmax))는 {max(tmax):.1f}입니다.")
+    print(f"연 최저 온도(min(tmin))는 {min(tmin):.1f}입니다.")
+
+if __name__ == "__main__":
+    main()
